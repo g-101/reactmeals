4088f217d778bea2ad2797e68a67a4ca949d5c09
genesis-g101 <gyreplicant@gmail.com>
2022-12-16 07:49:28 -0300

    add o state do modal

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -11,14 +11,16 @@ const Cart = props => {
   );
 
   return (
-    <Modal>
+    <Modal onClose={props.onClose}>
       {cartItems}
       <div className={classes.total}>
         <span>Total a pagar</span>
         <span>R$35.62</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes['button--alt']}>Voltar</button>
+        <button className={classes['button--alt']} onClick={props.onClose}>
+          Voltar
+        </button>
         <button className={classes.button}>Fechar Pedido</button>
       </div>
     </Modal>
