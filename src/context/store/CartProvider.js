92a6b1c2306044f543d4dfbcbcb2cb4a8571a30a
genesis-g101 <gyreplicant@gmail.com>
2022-12-19 10:12:07 -0300

    adicionando o cart reducer

--- src/context/store/CartProvider.js
+++ src/context/store/CartProvider.js
@@ -1,14 +1,39 @@
+import { useReducer } from 'react';
 import CartContext from './cartContext';
 
+const initialCartState = {
+  items: [],
+  totalAmount: 0,
+};
+const cartReducer = (state, action) => {
+  if (action.type === 'ADD') {
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+  return initialCartState;
+};
+
 const CartProvider = props => {
-  const addItemToCartHandler = item => {};
-  const removeItemToCartHandler = id => {};
+  const [cartState, dispatchCart] = useReducer(cartReducer, initialCartState);
+
+  const addItemToCartHandler = item => {
+    dispatchCart({ type: 'ADD', item: item });
+  };
+  const removeItemToCartHandler = id => {
+    dispatchCart({ type: 'REMOVE', id: id });
+  };
+
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemToCartHandler,
   };
+
   return <CartContext.Provider value={cartContext}>{props.children}</CartContext.Provider>;
 };
 
